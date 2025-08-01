ebab618d136f1ad84766a2ba53b294a9fde314fa
Yavonix <37795234+Yavonix@users.noreply.github.com>
2025-08-01 12:32:10 +1000

    formatted code

--- 2802ict-assignments/assignment 01/maze_src/maze.py
+++ 2802ict-assignments/assignment 01/maze_src/maze.py
@@ -2,21 +2,30 @@ import sys
 from dataclasses import dataclass, field
 from typing import Callable, Iterable
 
-from common import DepthLimitReachedError, NoSolutionError, NoSolutionError, Node, State, Maze_Common
+from common import (
+    DepthLimitReachedError,
+    NoSolutionError,
+    NoSolutionError,
+    Node,
+    State,
+    Maze_Common,
+)
+
 
 @dataclass
 class StackFrontier:
     frontier: list[Node] = field(default_factory=list)
 
-    def __init__(self, initial_nodes: list[Node]|None=None):
-        if initial_nodes: self.frontier=initial_nodes
+    def __init__(self, initial_nodes: list[Node] | None = None):
+        if initial_nodes:
+            self.frontier = initial_nodes
 
     def __contains__(self, state: State) -> bool:
         return any(node.state == state for node in self.frontier)
-    
+
     def empty(self):
         return len(self.frontier) == 0
-    
+
     def add(self, node: Node):
         self.frontier.append(node)
 
@@ -37,9 +46,10 @@ class StackFrontier:
     def size(self) -> int:
         return len(self.frontier)
 
+
 class Maze(Maze_Common):
 
-    def dfs_solve(self, max_depth=float('inf')) -> None:
+    def dfs_solve(self, max_depth=float("inf")) -> None:
         """Depth-limited depth-first search"""
 
         start_node = Node(state=self.start, parent=None, action=None, depth=0)
@@ -52,23 +62,27 @@ class Maze(Maze_Common):
         while not frontier.empty():
             node = frontier.pop()
             self.num_explored += 1
-            
+
             if node.state == self.goal:
                 self.solution = node
                 return
 
             self.explored.add(node.state)
-            
+
             for neighbour in self.neighbors(node):
                 if neighbour.depth > max_depth:
-                    cutoff_occurred=True
+                    cutoff_occurred = True
                     continue
-                if (neighbour.state not in self.explored and
-                    neighbour.state not in frontier):
+                if (
+                    neighbour.state not in self.explored
+                    and neighbour.state not in frontier
+                ):
                     frontier.add(neighbour)
 
-        if cutoff_occurred: raise DepthLimitReachedError
-        else: raise NoSolutionError
+        if cutoff_occurred:
+            raise DepthLimitReachedError
+        else:
+            raise NoSolutionError
 
     def ids_solve(self) -> None:
         """Depth-limited iterative depth-first search"""
@@ -84,8 +98,9 @@ class Maze(Maze_Common):
                 depth += 1
             except Exception as e:
                 raise e
-        
+
         raise DepthLimitReachedError
+
 
 if len(sys.argv) != 2:
     sys.exit("Usage: python maze.py maze.txt")
@@ -95,9 +110,12 @@ if __name__ == "__main__":
     m = Maze(sys.argv[1])
 
     m.reset_stats()
-    try: m.ids_solve()
-    except NoSolutionError: pass
-    except Exception as e: raise e
+    try:
+        m.ids_solve()
+    except NoSolutionError:
+        pass
+    except Exception as e:
+        raise e
     m.print()
 
     # print("Solution:\n\n")
